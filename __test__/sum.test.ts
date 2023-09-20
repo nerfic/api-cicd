3d29d396ce4c90cd3f397d06a4ea692ca0514a0a
Allan <allan.j@bealy.io>
2023-09-20 12:38:14 +0200

    add failing test

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -5,4 +5,8 @@ describe('sum function', () => {
     test('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
     });
+
+    test('adds 1 + 3 to equal 4', () => {
+        expect(sum(1, 2)).toBe(4);
+    });
 });
