81ccb9e827c9dcb3da97843c91dee5d7bf79ed91
Allan <allan.j@bealy.io>
2023-09-20 12:45:12 +0200

    Fix test error

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -7,10 +7,10 @@ describe('sum function', () => {
     });
 
     test('adds 1 + 1 to equal 2', () => {
-        expect(sum(1, 2)).toBe(2);
+        expect(sum(1, 1)).toBe(2);
     });
 
     test('adds 1 + 3 to equal 4', () => {
-        expect(sum(1, 2)).toBe(4);
+        expect(sum(1, 3)).toBe(4);
     });
 });
