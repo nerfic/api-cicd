cdd979d03f27e46afeea6bb97291d02a7cdd4e2e
Allan <allan.j@bealy.io>
2023-09-20 12:43:52 +0200

    Add test

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -6,6 +6,10 @@ describe('sum function', () => {
         expect(sum(1, 2)).toBe(3);
     });
 
+    test('adds 1 + 1 to equal 2', () => {
+        expect(sum(1, 2)).toBe(2);
+    });
+
     test('adds 1 + 3 to equal 4', () => {
         expect(sum(1, 2)).toBe(4);
     });
